bdedba6a9282ec34f9e2d3bcc54105a53cf9fe42
Elinda Sintaresmi <elinda.sinta@gmail.com>
2020-01-20 11:37:17 +0700

    edit form

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // welcome
 let getName = localStorage.getItem('Name')
 let addName = document.getElementById('welcome')
-addName.innerText= `Welcome ${getName}`
+addName.innerText = `Welcome ${getName}`
 
 
 
@@ -18,10 +18,10 @@ function register() {
     localStorage.setItem("Password", password)
 
     if (password.length >= 8 && email !== undefined && password !== undefined) {
-        
+
         window.location = "login.html"
     } else {
-        
+
         document.getElementById('error-message').innerHTML = 'Password should be more than 8 character'
     }
 }
@@ -32,12 +32,12 @@ function login() {
 
     let emailRegistered = localStorage.getItem('Email')
     let passwordRegistered = localStorage.getItem('Password')
-    
-  
+
+
     if (email === emailRegistered && password === passwordRegistered && email !== undefined && password !== undefined) {
-    
+
         window.location = "homepage.html"
-        
+
     } else {
         document.getElementById('error-message').innerHTML = 'Log in failed, please input your email and password !'
     }
@@ -50,7 +50,7 @@ function logout() {
     localStorage.removeItem('Password')
 
     let successLogout = alert('Log out sukses')
-    
+
     window.location = "index.html"
 }
 
